b59b64dc6351dcfc50918ab026963594b34f8802
weicl <chenglong.wei@mail.hypers.com>
2023-06-03 13:19:58 +0800

    docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -12,10 +12,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://yjgbg.github.io',
+  url: 'https://kepler.yjgbg.me',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/kepler/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
