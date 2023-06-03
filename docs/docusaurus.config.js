802891f38f586eaeda4b0b9a50bf9b1561415ffa
weicl <chenglong.wei@mail.hypers.com>
2023-06-03 12:51:53 +0800

    ..

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -15,12 +15,12 @@ const config = {
   url: 'https://yjgbg.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/kepler/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'yjgbg', // Usually your GitHub org/user name.
+  projectName: 'yjgbg.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
