e12f80509581787c56eb3507eb20cc9ff3a1b883
yjgbg <yjgbg1239461052@gmail.com>
2023-12-16 03:56:36 +0800

    hotReload功能

--- kepler-dsl/src/main/scala/build.scala
+++ kepler-dsl/src/main/scala/build.scala
@@ -8,22 +8,26 @@ object build:
     def apply(closure: _ >> project.type ?=> Unit) = new Plugin:
       override def apply(using ? >> project.type): Unit = closure.apply
   val project:"project" = compiletime.constValue
-  private def ~ = Plugin:
+  private def hotReload(args:Seq[String]) = Plugin:
     task(name := "~"):
-      action := {(p,args) =>
-        val cmdLine = ProcessHandle.current().nn.info().nn.commandLine().nn.get().nn
-        val newCmdLine = cmdLine.replaceAll("~[ \t]*"+args.mkString("[ \t]*"),args.mkString(" ")).nn
+      action := {p =>
+        val newCmdLine = ProcessHandle.current().nn.info().nn.commandLine().nn.get().nn
+          .replaceAll("~[ \t]*"+args.tail.mkString("[ \t]*"),args.tail.mkString(" ")).nn
+        println(newCmdLine)
         import scala.sys.process.*
         System.exit(newCmdLine.!)
       }
   def rootProject(args:Seq[String])(closure:Root >> project.type ?=> Unit):Unit = {
     if (args.isEmpty) println("no task to execute")
     given scope: >>[Root,project.type] = Scope.>>(collection.mutable.HashMap())
-    ~.apply
+    hotReload(args).apply
     closure.apply
     def executeTask(p: _ >> project.type,args:Seq[String]):Unit = 
       p.get(task).filter(it => it.get(name).get == args.head).headOption match
-        case Some(value) => value.get(action).foreach(_(p.asInstanceOf,args.tail))
+        case Some(value) => value.get(action).foreach:ac => 
+          value.get(dependsOn).foreach:n => 
+            executeTask(p,Seq(n))
+          ac(p.asInstanceOf)
         case None => p.get(project).filter(it => it.get(name).get == args.head).headOption match
           case Some(value) => executeTask(value,args.tail)
           case None => println("no task to execute")
@@ -35,7 +39,7 @@ object build:
   val task: "task" = compiletime.constValue
   given [A <: _ >> project.type]:MultiNodeKey[task.type,A,task.type] = Key.multiNodeKey
   val action: "action" = compiletime.constValue
-  given [A <: _ >> project.type]:SingleValueKey[action.type,A >> task.type,(A,Seq[String]) => Unit] = Key.singleValueKey
+  given [A <: _ >> project.type]:SingleValueKey[action.type,A >> task.type,A => Unit] = Key.singleValueKey
   val dependsOn: "dependsOn" = compiletime.constValue
   given [A <: 
     _ >> project.type
@@ -43,14 +47,14 @@ object build:
     ]:MultiValueKey[dependsOn.type,A,String] = Key.multiValueKey
 object buildSample:
   import build.{*,given}
-  val printName = Plugin:
-    task(name := "printName"):
-      dependsOn += "另一个task的名字"
-      action := { (p,args) => p.get(name) match
-        case None => println()
-        case Some(value) => println(value)
-      }
   @main def main(args:String*) = rootProject(args):
+    val printName = Plugin:
+      task(name := "printName"):
+        // dependsOn += "另一个task的名字"
+        action := { p => p.get(name) match
+          case None => println()
+          case Some(value) => println(value)
+        }
     name := "root"
     printName.apply
     project(name := "kepler-dsl"):
